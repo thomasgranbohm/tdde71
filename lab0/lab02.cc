271a44f477a1d591da47c84195600f03f50a80d5
vilma749 <vilma749@su04-215.ad.liu.se>
2026-09-09 10:35:58 +0200

    Small changes to lab02.cc

--- lab0/lab02.cc
+++ lab0/lab02.cc
@@ -1,4 +1,3 @@
-#include <format>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -6,8 +5,6 @@
 #include <vector>
 #include <iomanip>
 #include <algorithm> // for std::sort
-
-// What do you have to change if you omit "using namespace std;" ?
 
 struct Time
 {
@@ -18,18 +15,17 @@ struct Time
 public: // Set a default time to
     Time() : hours{0}, minutes{0}, seconds{0} {}
 
-    int getTotalSeconds() const
+    int GetTotalSeconds() const
     {
         return hours * 60 * 60 + minutes * 60 + seconds;
     }
 
-    std::string toString() const
+    std::string ToString() const
     {
         std::stringstream ss;
 
         // String building
-        ss << std::left
-           << std::setfill('0')
+        ss << std::setfill('0')
            << hours
            << ':'
            << std::right
@@ -39,9 +35,7 @@ public: // Set a default time to
            << std::setw(2)
            << seconds;
 
-        std::string s{};
-        std::getline(ss, s);
-        return s;
+        return ss.str();
     }
 };
 
@@ -51,17 +45,17 @@ struct Runner
     Time time;
 
 public:
-    Runner(const std::string &str) : name{"John Doe"}, time{}
+    Runner(const std::string &line) : name{"John Doe"}, time{}
     {
         // Init runner from full line
-        std::stringstream strstream{str};
-        strstream >> name;
-        strstream >> time.hours;
-        strstream >> time.minutes;
-        strstream >> time.seconds;
+        std::stringstream ss{line};
+        ss >> name;
+        ss >> time.hours;
+        ss >> time.minutes;
+        ss >> time.seconds;
     }
 
-    std::string toString() const
+    std::string ToString() const
     {
         std::string s;
         std::stringstream ss;
@@ -71,62 +65,111 @@ public:
            << std::setw(9)
            << name
            << " | "
-           << time.toString();
+           << time.ToString();
 
         std::getline(ss, s);
         return s;
     }
 };
 
+// std::ifstream open_file()
+// {
+//     // Get filename
+//     std::cout << "Ange filnamn: " << std::flush;
+//     std::string filename;
+//     std::cin >> filename;
+
+//     // Open file
+//     std::ifstream input_file{filename};
+
+//     // Print error message if file didn't open and restart function.
+//     if (input_file.is_open() == false)
+//     {
+//         std::cerr << "FEL: Filen gick inte att öppna!" << std::endl;
+//         return open_file(); // Recussion maxxing
+//     }
+
+//     return input_file;
+// }
+
+// int get_n_rows(int max)
+// {
+//     // Clear input buffer
+//     std::cin.clear();
+//     std::cin.ignore(1024, '\n');
+
+//     std::cout << "Ange antal rader: " << std::flush;
+//     int n_rows{};
+
+//     if (std::cin >> n_rows)
+//     {
+//         if (n_rows <= 0 || n_rows > max)
+//         {
+//             std::cerr << "FEL: Det finns inte " << n_rows << " rader i filen." << std::endl;
+//             return get_n_rows(max);
+//         }
+//     }
+//     else
+//     {
+//         std::cerr << "FEL: Inmatningen måste vara ett heltal!" << std::endl;
+//         return get_n_rows(max);
+//     }
+
+//     return n_rows;
+// }
+
 std::ifstream open_file()
 {
-    // Get filename
-    std::cout << "Ange filnamn: " << std::flush;
-    std::string filename;
-    std::cin >> filename;
-
-    // Open file
-    std::ifstream input_file(filename);
-
-    // Print error message if file didn't open and restart function.
-    if (input_file.is_open() == false)
+    while (true)
     {
-        std::cerr << "FEL: Filen gick inte att öppna!" << std::endl;
-        return open_file(); // Recussion maxxing
+        // Get filename
+        std::cout << "Ange filnamn: ";
+        std::string filename;
+        std::cin >> filename;
+    
+        // Open file
+        std::ifstream input_file{filename};
+    
+        // Print error message if file didn't open, otherwise return filestream
+        if (input_file.is_open())
+        {
+            return input_file;
+        }
+        std::cout << "FEL: Filen gick inte att öppna!" << std::endl;
     }
-
-    return input_file;
 }
 
 int get_n_rows(int max)
 {
-    // Clear input buffer
-    std::cin.clear();
-    std::cin.ignore(1024, '\n');
-
-    std::cout << "Ange antal rader: " << std::flush;
-    int n_rows{};
-
-    if (std::cin >> n_rows)
+    while(true)
     {
-        if (n_rows <= 0 || n_rows > max)
+        std::cin.clear();
+        std::cin.ignore(1024, '\n');
+
+        int n_rows{};
+
+        std::cout << "Ange antal rader: ";
+
+        if (!(std::cin >> n_rows))
         {
-            std::cerr << "FEL: Det finns inte " << n_rows << " rader i filen." << std::endl;
-            return get_n_rows(max);
+            std::cout << "FEL: Inmatningen måste vara ett heltal!" << std::endl;
+            continue;
         }
-    }
-    else
-    {
-        std::cerr << "FEL: Inmatningen måste vara ett heltal!" << std::endl;
-        return get_n_rows(max);
-    }
 
-    return n_rows;
+        if (n_rows < 1 || n_rows > max)
+        {
+            std::cout << "FEL: Det finns inte " << n_rows
+                      << " rader i filen." << std::endl;
+            continue;
+        }
+
+        return n_rows;
+    }
 }
 
 int main()
 {
-    std::vector<Runner> runners;
+    std::vector<Runner> runners; // List of runners to be sorted and printed
 
     std::ifstream input_file = open_file();
     std::string line;
@@ -142,14 +185,15 @@ int main()
     std::sort(runners.begin(), runners.end(),
               [](Runner a, Runner b)
               {
-                  return a.time.getTotalSeconds() < b.time.getTotalSeconds();
+                  // If true, put a before b
+                  return a.time.GetTotalSeconds() < b.time.GetTotalSeconds();
               });
 
     std::cout << "   Namn   |   Tid   \n"
               << "====================" << std::endl;
     for (int i = 0; i < n_rows; i++)
     {
-        std::cout << runners.at(i).toString() << std::endl;
+        std::cout << runners.at(i).ToString() << std::endl;
     }
 
     return 0;
