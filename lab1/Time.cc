089bed5d4ad4f8fae2e174674f29faf47f316b3a
vilman <vilman@fedora.local>
2026-09-13 13:55:01 +0200

    Added comments. Added more tests. Switched stringstream to (o/i)stringstream. Fixed a bug in Time::to_string.

--- lab1/Time.cc
+++ lab1/Time.cc
@@ -6,6 +6,7 @@
 #include <stdexcept>
 #include <vector>
 
+// Checks if time values are out of bounds/range
 void Time::check_values()
 {
     if (this->hour < 0 || this->hour >= 24)
@@ -28,20 +29,21 @@ void Time::check_values()
 
 Time::Time(int _hour, int _minute, int _second, int _milliseconds) : hour{_hour}, minute{_minute}, second{_second}, milliseconds{_milliseconds}
 {
-
     Time::check_values();
 }
 
-int get_next_integer(std::stringstream &ss, int n)
+// Returns the next n number of integers in an istringstream
+int get_next_integer(std::istringstream &iss, int n)
 {
     char c{};
-    std::stringstream cs;
+    std::ostringstream cs;
 
     for (int i = 0; i < n; i++)
     {
-        ss >> c;
+        iss >> c;
 
-        if (ss.fail())
+        // Throw if it failed to read or if the character was not an integer
+        if (iss.fail())
             throw std::logic_error(ERROR_VALID_INTEGER);
         else if (c < ASCII_ZERO || c > ASCII_NINE)
             throw std::logic_error(ERROR_ILLEGAL_CHAR);
@@ -52,11 +54,15 @@ int get_next_integer(std::stringstream &ss, int n)
     return std::stoi(cs.str());
 }
 
-void skip_char(std::stringstream &ss, char desired)
+// Skips the desired character in an istringstream by reading it
+// to a temporary variable 'c'
+void skip_char(std::istringstream &iss, char desired)
 {
     char c{};
-    ss >> c;
-    if (ss.fail())
+    iss >> c;
+
+    // Throw if it failed to read or if the read character was not the desired one
+    if (iss.fail())
         throw std::logic_error(ERROR_VALID_INTEGER);
     else if (c != desired)
         throw std::logic_error(ERROR_ILLEGAL_CHAR);
@@ -66,23 +72,27 @@ Time::Time(const std::string &str) : hour{0}, minute{0}, second{0}, milliseconds
 {
     int n_chars = str.size();
 
+    // Throw if the length of the string does not match an expected format.
+    // The expected formats are 'HH:MM:SS.mmm' and 'HH:MM:SS'.
     if (n_chars > 12 || n_chars < 8)
     {
         throw std::out_of_range(ERROR_ABNORMAL_TIMESTAMP);
     }
 
-    std::stringstream ss{str};
+    // Extract the time data from the string by using an istringstream:
+    std::istringstream iss{str};
 
-    this->hour = get_next_integer(ss, 2);
-    skip_char(ss, ASCII_COLON);
-    this->minute = get_next_integer(ss, 2);
-    skip_char(ss, ASCII_COLON);
-    this->second = get_next_integer(ss, 2);
+    this->hour = get_next_integer(iss, 2);
+    skip_char(iss, ASCII_COLON);
+    this->minute = get_next_integer(iss, 2);
+    skip_char(iss, ASCII_COLON);
+    this->second = get_next_integer(iss, 2);
 
+    // Extract the millisecond data if the string has the appropriate length
     if (n_chars > 8)
     {
-        skip_char(ss, ASCII_DOT);
-        this->milliseconds = get_next_integer(ss, 3);
+        skip_char(iss, ASCII_DOT);
+        this->milliseconds = get_next_integer(iss, 3);
     }
 
     check_values();
@@ -93,9 +103,14 @@ bool Time::is_am() const
     return this->hour < 12;
 }
 
+// Converts the time data to a string with one of the following formats:
+//  - HH:MM:SS - if twelve_hour == false and milliseconds == 0
+//  - HH:MM:SS(am/pm) - if twelve_hour == true and milliseconds == 0
+//  - HH:MM:SS.mmm - if twelve_hour == false and milliseconds != 0
+//  - HH:MM:SS.mmm(am/pm) - if twelve_hour == true and milliseconds != 0
 std::string Time::to_string(bool twelwe_hour) const
 {
-    std::stringstream ss{};
+    std::ostringstream oss{};
 
     bool is_am = this->is_am();
 
@@ -106,7 +121,7 @@ std::string Time::to_string(bool twelwe_hour) const
         hour = ((hour + 11) % 12) + 1;
     }
 
-    ss << std::setfill('0')
+    oss << std::setfill('0')
        << std::right
        << std::setw(2)
        << hour << ':'
@@ -117,28 +132,29 @@ std::string Time::to_string(bool twelwe_hour) const
 
     if (this->milliseconds != 0)
     {
-        ss << '.' << this->milliseconds;
+        oss << '.' << std::setw(3) << this->milliseconds;
     }
 
     if (twelwe_hour)
     {
-        ss << (is_am ? "am" : "pm");
+        oss << (is_am ? "am" : "pm");
     }
 
-    return ss.str();
+    return oss.str();
 }
 
+// Increases the time by one second.
 void Time::increment()
 {
-    if (++second == 60)
+    if (++this->second == 60)
     {
-        second %= 60;
-        if (++minute == 60)
+        this->second = 0;
+        if (++this->minute == 60)
         {
-            minute %= 60;
-            if (++hour == 24)
+            this->minute = 0;
+            if (++this->hour == 24)
             {
-                hour %= 24;
+                this->hour = 0;
             }
         }
     }
--- lab1/Time_test.cpp
+++ lab1/Time_test.cpp
@@ -1,6 +1,6 @@
 #include "catch.hpp"
 #include "Time.hpp"
-#include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -20,9 +20,13 @@ TEST_CASE("Constructors and getters")
         Time t1{12, 30, 30};
         Time t2{23, 59, 59};
 
+        // Out of bounds:
         CHECK_THROWS(Time{13, 35, 60});
         CHECK_THROWS(Time{13, 60, 35});
         CHECK_THROWS(Time{24, 35, 35});
+        CHECK_THROWS( Time{-1,35,35} );
+        CHECK_THROWS( Time{13,-1,35} );
+        CHECK_THROWS( Time{13,35,-1} );
 
         CHECK(t0.get_hour() == 0);
         CHECK(t0.get_minute() == 0);
@@ -35,15 +39,59 @@ TEST_CASE("Constructors and getters")
         CHECK(t2.get_second() == 59);
     }
 
+    SECTION("Integer + Millisecond")
+    {
+        Time t0{0, 0, 0, 0};
+        Time t1{12, 30, 30, 222};
+        Time t2{23, 59, 59, 999};
+
+        // Out of bounds:
+        CHECK_THROWS(Time{13, 35, 60, 100});
+        CHECK_THROWS(Time{13, 60, 35, 100});
+        CHECK_THROWS(Time{24, 35, 35, 100});
+        CHECK_THROWS(Time{13, 35, 35, 1000});
+        CHECK_THROWS(Time{13, 35, 35, 1000});
+        CHECK_THROWS(Time{13, 35, 35, 1000});
+        CHECK_THROWS(Time{-1, 35, 35, 100});
+        CHECK_THROWS(Time{35, -1, 35, 100});
+        CHECK_THROWS(Time{35, 35, -1, 100});
+        CHECK_THROWS(Time{35, 35, 35, -1});
+
+        CHECK(t0.get_hour() == 0);
+        CHECK(t0.get_minute() == 0);
+        CHECK(t0.get_second() == 0);
+        CHECK(t0.get_millisecond() == 0);
+        CHECK(t1.get_hour() == 12);
+        CHECK(t1.get_minute() == 30);
+        CHECK(t1.get_second() == 30);
+        CHECK(t1.get_millisecond() == 222);
+        CHECK(t2.get_hour() == 23);
+        CHECK(t2.get_minute() == 59);
+        CHECK(t2.get_second() == 59);
+        CHECK(t2.get_millisecond() == 999);
+    }
+
     SECTION("String")
     {
         Time t0{"00:00:00"};
         Time t1{"12:30:30"};
         Time t2{"23:59:59"};
 
+        // Out of bounds:
         CHECK_THROWS(Time{"13:35:60"});
         CHECK_THROWS(Time{"13:60:35"});
         CHECK_THROWS(Time{"24:35:35"});
+        CHECK_THROWS(Time{"-13:35:35"});
+        CHECK_THROWS(Time{"13:-35:35"});
+        CHECK_THROWS(Time{"13:35:-35"});
+
+        // Wrong format:
+        CHECK_THROWS(Time{"123::456"});
+        CHECK_THROWS(Time{"12:34"});
+        CHECK_THROWS(Time{"12.34.56"});
+        CHECK_THROWS(Time("12:34:56.789.10"));
+        CHECK_THROWS(Time("12:34:56.789:10"));
+        CHECK_THROWS(Time("12:34:56.7.89"));
 
         CHECK(t0.get_hour() == 0);
         CHECK(t0.get_minute() == 0);
@@ -54,6 +102,47 @@ TEST_CASE("Constructors and getters")
         CHECK(t2.get_hour() == 23);
         CHECK(t2.get_minute() == 59);
         CHECK(t2.get_second() == 59);
+    }
+    SECTION("String + Millisecond")
+    {
+        Time t0{"00:00:00.000"};
+        Time t1{"12:30:30.222"};
+        Time t2{"23:59:59.999"};
+
+        // Out of bounds:
+        CHECK_THROWS(Time{"13:35:60.100"});
+        CHECK_THROWS(Time{"13:60:35.100"});
+        CHECK_THROWS(Time{"24:35:35.100"});
+        CHECK_THROWS(Time{"13:35:35.1000"});
+        CHECK_THROWS(Time{"13:35:35.1000"});
+        CHECK_THROWS(Time{"13:35:35.1000"});
+        CHECK_THROWS(Time{"-13:35:35.100"});
+        CHECK_THROWS(Time{"13:-35:35.100"});
+        CHECK_THROWS(Time{"13:35:-35.100"});
+        CHECK_THROWS(Time{"13:35:35.-100"});
+
+        // Wrong format:
+        CHECK_THROWS(Time{"123::456.100"});
+        CHECK_THROWS(Time{"12:34:56:100"});
+        CHECK_THROWS(Time{"12.34.56.100"});
+        CHECK_THROWS(Time{"12.34.56:100"});
+        CHECK_THROWS(Time{"12:3456.100"});
+        CHECK_THROWS(Time{"12.34.56.10"});
+        CHECK_THROWS(Time{"12.34.5:100"});
+        CHECK_THROWS(Time{"12:3456.100"});
+
+        CHECK(t0.get_hour() == 0);
+        CHECK(t0.get_minute() == 0);
+        CHECK(t0.get_second() == 0);
+        CHECK(t0.get_millisecond() == 0);
+        CHECK(t1.get_hour() == 12);
+        CHECK(t1.get_minute() == 30);
+        CHECK(t1.get_second() == 30);
+        CHECK(t1.get_millisecond() == 222);
+        CHECK(t2.get_hour() == 23);
+        CHECK(t2.get_minute() == 59);
+        CHECK(t2.get_second() == 59);
+        CHECK(t2.get_millisecond() == 999);
     }
 }
 
@@ -72,44 +161,48 @@ TEST_CASE("to_string")
     Time t2{12, 0, 0};
     Time t3{13, 0, 0};
     Time t4{23, 59, 59};
+    Time t5{12, 34, 56, 78};
+    Time t6{12, 34, 56, 0};
+    Time t7{"12:34:56.078"};
+    Time t8{"12:34:56.000"};
     SECTION("24 hour format no argument")
     {
         CHECK(t0.to_string() == "00:00:00");
+        CHECK(t1.to_string() == "11:59:59");
+        CHECK(t2.to_string() == "12:00:00");
+        CHECK(t3.to_string() == "13:00:00");
+        CHECK(t4.to_string() == "23:59:59");
+        CHECK(t5.to_string() == "12:34:56.078");
+        CHECK(t6.to_string() == "12:34:56");
+        CHECK(t7.to_string() == "12:34:56.078");
+        CHECK(t8.to_string() == "12:34:56");
     }
 
-    // SECTION("24 hour format with argument")
-    // {
-    // }
+    SECTION("24 hour format with argument")
+    {
+        CHECK(t0.to_string(false) == "00:00:00");
+        CHECK(t1.to_string(false) == "11:59:59");
+        CHECK(t2.to_string(false) == "12:00:00");
+        CHECK(t3.to_string(false) == "13:00:00");
+        CHECK(t4.to_string(false) == "23:59:59");
+        CHECK(t5.to_string(false) == "12:34:56.078");
+        CHECK(t6.to_string(false) == "12:34:56");
+        CHECK(t7.to_string(false) == "12:34:56.078");
+        CHECK(t8.to_string(false) == "12:34:56");
+    }
 
-    // SECTION("12 hour format")
-    // {
-    // }
-}
-
-TEST_CASE("Time stamps")
-{
-
-    Time a{4, 50, 20};
-    Time b{15, 50, 20};
-    Time c{0, 30, 10};
-    Time d{12, 30, 10};
-
-    CHECK(a.to_string() == "04:50:20");
-    CHECK(a.to_string(true) == "04:50:20am");
-    CHECK(b.to_string() == "15:50:20");
-    CHECK(b.to_string(true) == "03:50:20pm");
-    CHECK(c.to_string() == "00:30:10");
-    CHECK(c.to_string(true) == "12:30:10am");
-    CHECK(d.to_string() == "12:30:10");
-    CHECK(d.to_string(true) == "12:30:10pm");
-
-    Time e{12, 30, 10, 300};
-    CHECK(e.to_string() == "12:30:10.300");
-    CHECK(e.to_string(true) == "12:30:10.300pm");
-
-    Time f{"12:30:10.300"};
-    CHECK(f.to_string() == "12:30:10.300");
-    CHECK(f.to_string(true) == "12:30:10.300pm");
+    SECTION("12 hour format")
+    {
+        CHECK(t0.to_string(true) == "12:00:00am");
+        CHECK(t1.to_string(true) == "11:59:59am");
+        CHECK(t2.to_string(true) == "12:00:00pm");
+        CHECK(t3.to_string(true) == "01:00:00pm");
+        CHECK(t4.to_string(true) == "11:59:59pm");
+        CHECK(t5.to_string(true) == "12:34:56.078pm");
+        CHECK(t6.to_string(true) == "12:34:56pm");
+        CHECK(t7.to_string(true) == "12:34:56.078pm");
+        CHECK(t8.to_string(true) == "12:34:56pm");
+    }
 }
 
 TEST_CASE("Time stamp creation")
@@ -158,6 +251,7 @@ TEST_CASE("Operators")
         Time a{13, 37, 0, 400};
         CHECK((++a).to_string() == "13:37:01.400");
         CHECK((a++).to_string() == "13:37:01.400");
+        CHECK(a.to_string() == "13:37:02.400");
 
         // Check minute passing
         Time b{13, 37, 59};
@@ -186,17 +280,33 @@ TEST_CASE("Operators")
         Time b{17, 38, 0};
 
         CHECK(a < b);
-        CHECK((a > b) == false);
+        CHECK_FALSE(a > b);
         CHECK(a <= b);
-        CHECK((a >= b) == false);
+        CHECK_FALSE(a >= b);
 
         CHECK(a == a);
         CHECK(a != b);
     }
     SECTION("Streaming")
     {
-        Time a{13, 37, 20, 123};
-        cout << a << endl; // TODO: Känns fel typ
-        // FIXME: Blir fel hehehe
+        Time t0{};
+        Time t1{1, 2, 3};
+        Time t2{1, 2, 3, 4};
+        ostringstream oss{};
+        SECTION("Test 1")
+        {
+            oss << t0;
+            CHECK(oss.str() == t0.to_string());
+        }
+        SECTION("Test 2")
+        {
+            oss << t1;
+            CHECK(oss.str() == t1.to_string());
+        }
+        SECTION("Test 3")
+        {
+            oss << t2;
+            CHECK(oss.str() == t2.to_string());
+        }
     }
 }
